836e33ebead9f733653c5868f73077e7351e6f2d
Cariad Eccleston <cariad@hey.com>
2021-03-22 14:34:29 +0000

    Add bashrc.sh

---
+++ data/bashrc.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+GPG_TTY="$(tty)"
+export GPG_TTY
+
+export PIPENV_VENV_IN_PROJECT=1
+
+export PYENV_ROOT="${HOME:?}/.pyenv"
+export PYENV_VERSION=3.9.2
+
+export PATH="${PYENV_ROOT:?}/bin:${PATH:?}"
+
+eval "$(pyenv init -)"
