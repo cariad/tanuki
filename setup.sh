3ebb2d58517ae116919eafb07e00cbc270a52938
Cariad Eccleston <cariad@hey.com>
2021-03-22 15:42:20 +0000

    Fix home path

--- setup.sh
+++ setup.sh
@@ -5,6 +5,15 @@ set -e
 li="\033[1;34m↪\033[0m "  # List item
 ok="\033[0;32m✔️\033[0m "  # OK
 
+user="$(logname)"
+echo -e "${li:?} User:            ${user:?}"
+
+home="/home/${user:?}"
+echo -e "${li:?} Home:            ${home:?}"
+
+pyenv_dir="${home:?}/.pyenv"
+echo -e "${li:?} pyenv directory: ${pyenv_dir:?}"
+
 echo -e "${li:?} Updating..."
 apt update --yes
 
@@ -28,19 +37,29 @@ systemctl enable docker
 echo -e "${li:?} Starting Docker..."
 systemctl start docker
 
-user="$(logname)"
 echo -e "${li:?} Adding ${user:?} to Docker group..."
 gpasswd -a "${user:?}" docker
 
-echo -e "${li:?} Installing pyenv..."
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-cd ~/.pyenv && src/configure && make -C src
 
-tanukirc=". ~/tanuki/data/bashrc.sh"
+if [ -d "${pyenv_dir:?}" ]
+then
+    echo "Directory /path/to/dir exists."
+else
+  echo -e "${li:?} Installing pyenv..."
+  git clone https://github.com/pyenv/pyenv.git "${pyenv_dir:?}"
+fi
 
-if ! grep -q "${tanukirc:?}" ~/.bashrc; then
+pushd "${pyenv_dir:?}"
+src/configure
+make -C src
+popd
+
+tanukirc=". ${home:?}/tanuki/data/bashrc.sh"
+bashrc_path="${home:?}/.bashrc"
+
+if ! grep -q "${tanukirc:?}" "${bashrc_path:?}"; then
   echo -e "${li:?} Referencing: ${tanukirc:?}"
-  echo ". ~/tanuki/data/bashrc.sh" >> ~/.bashrc
+  echo "${tanukirc:?}" >> "${bashrc_path:?}"
 else
   echo -e "${li:?} tanukirc already referenced."
 fi
