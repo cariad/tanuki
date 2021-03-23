d9c4d7d74880846c32f3a44b2bcf01772365c3cf
Cariad Eccleston <cariad@hey.com>
2021-03-23 13:49:51 +0000

    WIP

--- scripts/install-pyenv.sh
+++ scripts/install-pyenv.sh
@@ -4,12 +4,12 @@ set -e
 
 if [ -d ~/.pyenv ]; then
   echo -e "${li:?}Updating pyenv..."
-  cd ~/.pyenv
+  pushd ~/.pyenv
   git pull --ff-only
 else
   echo -e "${li:?}Downloading pyenv..."
   git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-  cd ~/.pyenv
+  pushd ~/.pyenv
 fi
 
 eval "$(pyenv init -)"
@@ -17,6 +17,6 @@ eval "$(pyenv init -)"
 echo -e "${li:?}Building pyenv..."
 src/configure
 make -C src
-cd ..
+popd
 
 echo -e "${ok:?}Installed pyenv!"
