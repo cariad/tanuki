665a625689a0666ba14794e8185c5ecdc465687b
Cariad Eccleston <cariad@hey.com>
2021-03-23 13:46:51 +0000

    WIP

--- scripts/configure-known-hosts.sh
+++ scripts/configure-known-hosts.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+
+if [ -f ~/.ssh/known_hosts ]; then
+  echo -e "${ok:?}\"~/.ssh/known_hosts\" already exists."
+  exit 0
+fi
+
 echo -e "${li:?}Configuring known_hosts..."
 ln data/known_hosts ~/.ssh/known_hosts
 echo -e "${ok:?}Configured known_hosts!"
