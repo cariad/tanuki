bc6af854883256b81301f510b4650d4849e78447
Cariad Eccleston <cariad@hey.com>
2021-03-23 15:17:29 +0000

    WIP

--- scripts/populate-configuration.sh
+++ scripts/populate-configuration.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 set -e
-
-echo -e "${li:?}Importing AWS CLI key..."
-envsubst < ./data/.gitconfig > ./data/.gitconfig.filled
-envsubst < ./data/gpg-identity > ./data/gpg-identity.filled
-
-echo -e "${ok:?}Installed AWS CLI!"
+echo -e "${li:?}Populating configuration..."
+envsubst < data/.gitconfig   > data/.gitconfig.filled
+envsubst < data/gpg-identity > data/gpg-identity.filled
+echo -e "${ok:?}Populated configuration!"
 set +e
