951872a424e30fbd704f5db49175c7ca93bfd82f
Cariad Eccleston <cariad@hey.com>
2021-03-23 10:28:50 +0000

    WIP

--- scripts/import-gpg-key.sh
+++ scripts/import-gpg-key.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-if [ ! -f private.key ]; then
+key_path=../data/private.key
+
+if [ ! -f "${key_path:?}" ]; then
   echo -e "${ok:?}No private GPG key to import."
   exit
 fi
@@ -11,8 +13,8 @@ echo -e "${li:?}You'll be prompted to enter your private GPG key passphrase."
 echo -e "${li:?}This is required in order to import your key."
 echo -e "${li:?}Press return to continue... "
 read -r
-gpg --import private.key
-rm -f        private.key
+gpg --import "${key_path:?}"
+rm -f        "${key_path:?}"
 
 echo -e "${li:?}Configuring git signing key..."
 key_id="$(gpg --with-colons --list-secret-keys |
